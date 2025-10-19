ebd48f6d17018e4638bed3e7946f0975d40a47bf
redisnotbluedev <ededmaude@gmail.com>
2025-10-19 12:44:21 +1030

    smash lexer

---
+++ source/_ext/smash_lexer.py
@@ -0,0 +1,11 @@
+from pygments.lexers.shell import BashLexer
+
+# TOTALLY not reskinned Bash
+class SmashLexer(BashLexer):
+    name = "Smash"
+    aliases = ["smash"]
+    filenames = ["*.sm", "*.smash"]
+    mimetypes = ["text/x-smash"]
+
+def setup(app):
+	app.add_lexer("Smash", SmashLexer)
